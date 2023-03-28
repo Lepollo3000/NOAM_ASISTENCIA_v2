24987a1d9167a539071e8ebbddd11870a22d8d46
jlepe <josemanuel.lepe@gmail.com>
2023-03-28 12:53:26 -0600

    Añadida y completada funcionalidad para breadcrumbs. Agregado título en cada pantalla creada anteriormente.

--- NOAM_ASISTENCIA_v2/Client/Pages/Administrador/Edit.razor.cs
+++ NOAM_ASISTENCIA_v2/Client/Pages/Administrador/Edit.razor.cs
@@ -1,6 +1,27 @@
-﻿namespace NOAM_ASISTENCIA_v2.Client.Pages.Administrador
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using NOAM_ASISTENCIA_v2.Client.Shared;
+using NOAM_ASISTENCIA_v2.Shared.Models;
+
+namespace NOAM_ASISTENCIA_v2.Client.Pages.Administrador
 {
     partial class Edit
     {
+        [CascadingParameter] public MainLayout Layout { get; set; } = null!;
+        [Parameter] public int ServicioId { get; set; }
+
+        private SucursalServicio _model = null!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            List<BreadcrumbItem> breadcrumb = new List<BreadcrumbItem>()
+            {
+                new BreadcrumbItem("Home", href: ""),
+                new BreadcrumbItem("Servicios", href: "/servicios"),
+                new BreadcrumbItem("Editar", href: $"/servicios/edit/{ServicioId}")
+            };
+
+            await Layout.SetBreadcrumb(breadcrumb);
+        }
     }
 }
