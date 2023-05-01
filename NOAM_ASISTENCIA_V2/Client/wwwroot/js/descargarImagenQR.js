cf9f886b5eba0f02e7c512b8b9550d737884334d
jlepe <josemanuel.lepe@gmail.com>
2023-05-01 09:53:51 -0600

    Añadida funcionalidad para generar y descargar QR's de cada servicio desde la aplicación.

---
+++ NOAM_ASISTENCIA_V2/Client/wwwroot/js/descargarImagenQR.js
@@ -0,0 +1,10 @@
+﻿function descargarImagenQR(fileName, content) {
+    let link = document.createElement('a');
+
+    link.download = fileName;
+    link.href = 'data:image/png;base64,' + encodeURIComponent(content);
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
--- NOAM_ASISTENCIA_v2/Client/Pages/Administrador/Servicios/Index.razor.cs
+++ NOAM_ASISTENCIA_v2/Client/Pages/Administrador/Servicios/Index.razor.cs
@@ -1,11 +1,13 @@
 ﻿using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.JSInterop;
 using MudBlazor;
 using NOAM_ASISTENCIA_V2.Client.Shared;
 using NOAM_ASISTENCIA_V2.Client.Utils.Features;
 using NOAM_ASISTENCIA_V2.Shared.Models;
 using NOAM_ASISTENCIA_V2.Shared.RequestFeatures;
+using QRCoder;
 using System.Net.Http.Json;
 using System.Text.Json;
 
@@ -18,12 +20,13 @@ partial class Index
 
     [Inject] private HttpClient HttpClient { get; set; } = null!;
     [Inject] private SweetAlertService SwalService { get; set; } = null!;
+    [Inject] private IJSRuntime JsRuntime { get; set; } = null!;
 
     private readonly string _allItemsText = "Mostrando {first_item} de {last_item}. Total: {all_items}";
     private readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
     private readonly int[] _pageSizeOption = { 5, 10, 15, 20 };
 
-    private int _pageSize = 5;
+    private int _pageSize = 10;
     private ServicioDTO _model = new();
     private SearchParameters _searchParameters = new();
     private MudTable<ServicioDTO> _table = new();
@@ -36,10 +39,10 @@ partial class Index
     private async Task InitializeBreadcrumb()
     {
         List<BreadcrumbItem> breadcrumb = new()
-            {
-                new BreadcrumbItem("Inicio", href: ""),
-                new BreadcrumbItem("Servicios", href: "servicios")
-            };
+        {
+            new BreadcrumbItem("Inicio", href: ""),
+            new BreadcrumbItem("Servicios", href: "servicios")
+        };
 
         await Layout.SetBreadcrumb(breadcrumb);
     }
@@ -100,6 +103,91 @@ partial class Index
     private async Task ModificarEstatusRegistro(ServicioDTO registro)
     {
         await ConfirmAlert(registro);
+    }
+
+    private async Task DownloadQRCode(int servicioId, string codigoId)
+    {
+        await DownloadQRCodeAlert(servicioId, codigoId);
+    }
+
+    private async Task DownloadQRCodeAlert(int servicioId, string codigoId)
+    {
+        await SwalService.FireAsync(new SweetAlertOptions
+        {
+            Title = "Generando código QR... Espere",
+            Html = $@"<div class=""mx-4 my-5"" style=""text-align: center"">
+                    <i class=""text-info fa fa-sync-alt fa-4x fa-spin""></i>
+                </div>",
+            ShowConfirmButton = false,
+            ShowCancelButton = false,
+            AllowEscapeKey = false,
+            AllowEnterKey = false,
+            AllowOutsideClick = false,
+            DidOpen = new SweetAlertCallback(async () =>
+            {
+                // POR ALGUNA PENDEJA RAZÓN SI NO SE PONE UN DELAY NO SE MUESTRA LA ALERTA
+                await Task.Delay(250);
+
+                bool successful = await GenerateQRCode(servicioId, codigoId);
+
+                if (successful)
+                {
+                    await SuccessfulDownloadAlert();
+                }
+            })
+        });
+    }
+
+    private async Task<bool> GenerateQRCode(int servicioId, string codigoId)
+    {
+        try
+        {
+            QRCodeGenerator qrCodeGenerate = new();
+
+            // SE OBTIENEN LOS DATOS PARA CODIFICAR EL CÓDIGO QR
+            QRCodeData qrCodeData = qrCodeGenerate.CreateQrCode(
+                plainText: servicioId.ToString(),
+                eccLevel: QRCodeGenerator.ECCLevel.Q
+            );
+
+            BitmapByteQRCode qrCode = new(qrCodeData);
+
+            // SE OBTIENE EL GRÁFICO DEL CÓDIGO QR
+            byte[] qrBitMap = qrCode.GetGraphic(20);
+
+            // SE TRANSFORMA EL ARREGLO DE BYTES EN TEXTO PLANO CODIFICADO EN BASE 64
+            string base64 = Convert.ToBase64String(qrBitMap);
+
+            // INVOCA UNA FUNCION DE JS PARA DESCARGAR LA IMAGEN GENERADA DEL CODIGO QR
+            await JsRuntime.InvokeAsync<object>("descargarImagenQR", $"QR_{codigoId}", base64);
+
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
+
+    private async Task SuccessfulDownloadAlert()
+    {
+        string confirmButtonColor = Theme.Palette.Primary.Value;
+
+        await SwalService.FireAsync(new SweetAlertOptions
+        {
+            Icon = SweetAlertIcon.Success,
+            Title = "Descarga exitosa",
+            Html = $@"<div class=""mx-4 my-3"" style=""text-align: center"">
+                    Código QR generado exitosamente.
+                </div>",
+            ShowConfirmButton = true,
+            ConfirmButtonColor = confirmButtonColor,
+            ConfirmButtonText = "Entendido",
+            DidOpen = new SweetAlertCallback(() =>
+            {
+                StateHasChanged();
+            })
+        });
     }
 
     private async Task ConfirmAlert(ServicioDTO registro)
